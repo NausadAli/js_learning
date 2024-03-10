e2666ebcdedefc2837b7f3b160f46c9b444cb9fb
Nousad Ali <121709928+NausadAli@users.noreply.github.com>
2024-03-10 11:07:30 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+//3 types of Variables..
+// const wherea everyThing is Constant and we can't Update it
+
+const accountId = 3423232 //Not Changeable
+let accountName = "Nousad Ali" //changeAble
+// var accountCity - Don't use the var in JavaScript it has problems with Block Scopes and Functional Scope
+
+accountCity = 'Jaipur' // not a GOOD approach. Changeable
+
+let phnNo;
+
+console.log(accountId);
+// accountId = 9090
+// Console.table - here we can write all the output in one place a
+console.table([accountId, accountName, accountCity])
+accountCity = 'Kolkata'
+accountName = "Parul Mohapatra"
+console.table([accountId, accountName, accountCity])
+
