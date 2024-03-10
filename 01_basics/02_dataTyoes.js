5e0b4748897be1dcb5f7c4bd08dcb0e13a8d21e6
Nousad Ali <121709928+NausadAli@users.noreply.github.com>
2024-03-10 14:25:52 +0000

    datatypes config done

---
+++ 01_basics/02_dataTyoes.js
@@ -0,0 +1,27 @@
+let name = "Nousad"
+
+// number -- goes from 2^53 or 2^52
+// bigint for bigger than number 
+
+// string => " "
+// boolean => true/false
+// null => standeAlone value (Object 🤯) 
+
+/*
+    it means dabba hai variable ka but wo dabba khali hai..
+    let state = null
+
+*/
+// undefined => is undefined
+
+/*
+    undefined means we didn't assign the varable
+*/
+
+// symbol  //symbol is for uniqueness mostly in React
+
+
+// OBJECT (!important )
+
+console.log(typeof null);       //🤯 object
+console.log(typeof undefined);  //undefined
