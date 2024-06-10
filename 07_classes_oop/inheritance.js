941f31e9d7e593a478ac126a29aefde28a00e25c
Nousad Ali <121709928+NausadAli@users.noreply.github.com>
2024-06-10 10:14:45 +0000

    Static KeyWord

--- 07_classes_oop/inheritance.js
+++ 07_classes_oop/inheritance.js
@@ -1,29 +1,33 @@
-class User{
-    constructor(username){
-         this.username = username;
-    }
+class User {
+     constructor(username) {
+          this.username = username;
+     }
 
-    findUser(){
-         return this.username
-    }
+     findUser() {
+          return this.username
+     }
 }
 
-class Teacher extends User{
-    constructor(username, email,password){
-         super(username)     //Same as fn.call(this, Username)
-         this.email = email;
-         this.password = password
-    }
+class Teacher extends User {
+     constructor(username, email, password) {
+          super(username)     //Same as fn.call(this, Username)
+          this.email = email;
+          this.password = password
+     }
 
-    addCourse(){
-         return 'A new Course is Added'
-    }
+     addCourse() {
+          return 'A new Course is Added'
+     }
+     static didNotAccessed() {
+          console.log("You can't access this part")
+     }
 }
 
 
-const teach1 = new Teacher('Aftab Ali','amiAftab@gmail.com','394895u000');
+const teach1 = new Teacher('Aftab Ali', 'amiAftab@gmail.com', '394895u000');
 
 console.log(teach1.findUser())
+// teach1.didNotAccessed()//You can't accesss this This Gives Error
 
 const tea = new User('Hello');
 // console.log(tea.addCourse())       //This gives error 
