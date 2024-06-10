625771969cecb71df4d2ba0a0d469d1665483ed9
Nousad Ali <121709928+NausadAli@users.noreply.github.com>
2024-06-10 10:01:50 +0000

    Inheritance Done.

---
+++ 07_classes_oop/inheritance.js
@@ -0,0 +1,31 @@
+class User{
+    constructor(username){
+         this.username = username;
+    }
+
+    findUser(){
+         return this.username
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email,password){
+         super(username)     //Same as fn.call(this, Username)
+         this.email = email;
+         this.password = password
+    }
+
+    addCourse(){
+         return 'A new Course is Added'
+    }
+}
+
+
+const teach1 = new Teacher('Aftab Ali','amiAftab@gmail.com','394895u000');
+
+console.log(teach1.findUser())
+
+const tea = new User('Hello');
+// console.log(tea.addCourse())       //This gives error 
+
+console.log(teach1 instanceof User)
